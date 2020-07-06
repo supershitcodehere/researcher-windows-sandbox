fc9de769b0771878e9eb3fb6ea95a941e41a7a46
39ff <discover@cse.ne.jp>
2020-07-06 21:21:57 +0900

    added firewall policy and squid

--- scripts/login.bat
+++ scripts/login.bat
@@ -1,3 +1,15 @@
+@setlocal enabledelayedexpansion
+set INI_FILE_FULLPATH=C:\users\WDAGUtilityAccount\Desktop\shared\account.ini
+call C:\users\WDAGUtilityAccount\Desktop\scripts\GetIni.bat :READ_INI_VAL "NORDVPN" "VPNUSERNAME" NORDVPNUSERNAME %INI_FILE_FULLPATH%
+call C:\users\WDAGUtilityAccount\Desktop\scripts\GetIni.bat :READ_INI_VAL "NORDVPN" "VPNPASSWORD" NORDVPNPASSWORD %INI_FILE_FULLPATH%
+
+curl -L "http://packages.diladele.com/squid/3.5.28/squid.msi" --output C:\users\WDAGUtilityAccount\Desktop\squid.msi
+C:\users\WDAGUtilityAccount\Desktop\squid.msi /quiet /norestart
+del C:\users\WDAGUtilityAccount\Desktop\squid.msi
+del C:\Squid\etc\squid\squid.conf
+copy "C:\users\WDAGUtilityAccount\Desktop\scripts\squid.conf" "C:\Squid\etc\squid\squid.conf"
+powershell Restart-Service -Name 'Squid for Windows'
+
 curl -L "https://dist.torproject.org/torbrowser/9.5.1/tor-win64-0.4.3.5.zip" --output C:\users\WDAGUtilityAccount\Desktop\tor.zip
 powershell Expand-Archive C:\users\WDAGUtilityAccount\Desktop\tor.zip -DestinationPath C:\users\WDAGUtilityAccount\Desktop\tor
 del C:\users\WDAGUtilityAccount\Desktop\tor.zip
@@ -10,5 +22,8 @@ copy "C:\users\WDAGUtilityAccount\Desktop\scripts\firefox-secure-profile.bat" "C
 
 powershell Set-ExecutionPolicy RemoteSigned
 powershell -Fil "C:\users\WDAGUtilityAccount\Desktop\scripts\network.ps1"
+
+start C:\users\WDAGUtilityAccount\Desktop\tor\Tor\tor.exe HardwareAccel 1 ClientOnly 1 StrictNodes 1 ExcludeNodes "{ch},{jp},{us},{gb},{ca},{au},{nz},{dk}" HTTPSProxy 89.187.161.86:80 HTTPSProxyAuthenticator "%NORDVPNUSERNAME%:%NORDVPNPASSWORD%"
+
 
 pause
--- scripts/network.ps1
+++ scripts/network.ps1
@@ -3,4 +3,4 @@ Set-NetFirewallProfile -Name Private -DefaultOutboundAction Block
 Set-NetFirewallProfile -Name Public -DefaultOutboundAction Block
 
 Remove-NetFirewallRule -Direction Outbound
-New-NetFirewallRule -DisplayName "NordVPN" -Direction Outbound -LocalPort Any -Protocol Any -Action Allow -RemoteAddress 89.187.161.149
+New-NetFirewallRule -DisplayName "NordVPN JP HTTP Proxy" -Direction Outbound -LocalPort Any -Protocol Any -Action Allow -RemoteAddress 89.187.161.86
